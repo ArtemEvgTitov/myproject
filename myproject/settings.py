c66818fe1f77d4f9547f24deb45f14c99da7705f
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-12-25 22:01:50 +0300

    fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 
 ALLOWED_HOSTS = [
     'artemtitov.pythonanywhere.com'
